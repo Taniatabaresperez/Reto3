717a463213ac531f51d23e77f073f70164f42c30
Tania Tabares Perez <taniatabaresperez@gmail.com>
2021-10-21 20:45:17 -0500

    modificaciones en el main

--- src/main/java/Reto3_Servicios_Medicos/Reto3_Servicios_Medicos/Reto3ServiciosMedicosApplication.java
+++ src/main/java/Reto3_Servicios_Medicos/Reto3_Servicios_Medicos/Reto3ServiciosMedicosApplication.java
@@ -9,8 +9,11 @@ package Reto3_Servicios_Medicos.Reto3_Servicios_Medicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"Reto3_Servicios_Medicos.Reto3_Servicios_Medicos.modelo"})
 @SpringBootApplication
 public class Reto3ServiciosMedicosApplication {
 
